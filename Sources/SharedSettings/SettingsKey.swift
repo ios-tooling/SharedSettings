8e48e72c0e4e77004115e5d5939739c87c82fb51
Ben Gottlieb <ben@standalone.com>
2026-06-21 08:21:28 -0500

    removed Codable requirement from SettingsKey.Payload

--- Sources/SharedSettings/SettingsKey.swift
+++ Sources/SharedSettings/SettingsKey.swift
@@ -10,15 +10,26 @@ import Foundation
 public enum SettingsLocation: Sendable { case userDefaults, cloudKit, keychain, memory }
 
 public protocol SettingsKey<Payload>: Sendable {
-	associatedtype Payload: Codable & Sendable
+	associatedtype Payload: Sendable
 	static var defaultValue: Payload { get }
 	static var name: String { get }
 	static var location: SettingsLocation { get }
-	
+
 	nonisolated static func from(userDefaults: UserDefaults) -> Payload?
 	nonisolated static func set(_ value: Payload?, in userDefaults: UserDefaults)
 	nonisolated static func fromCloudKit() -> Payload?
 	nonisolated static func setInCloudKit(_ value: Payload?)
+	nonisolated static func fromKeychain() -> Payload?
+	nonisolated static func setInKeychain(_ value: Payload?)
+}
+
+public extension SettingsKey {
+	static func from(userDefaults: UserDefaults) -> Payload? { nil }
+	static func set(_ value: Payload?, in userDefaults: UserDefaults) { }
+	static func fromCloudKit() -> Payload? { nil }
+	static func setInCloudKit(_ value: Payload?) { }
+	static func fromKeychain() -> Payload? { nil }
+	static func setInKeychain(_ value: Payload?) { }
 }
 
 extension SettingsKey {
